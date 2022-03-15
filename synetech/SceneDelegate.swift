8a008173df571f457339bacc71fe2d9b83b35aa8
Patrik Hora <hora.patrik@centrum.cz>
2022-03-15 13:30:06 +0100

    Remake the project file structure to fix weird issues

---
+++ synetech/SceneDelegate.swift
@@ -0,0 +1,22 @@
+//
+// SceneDelegate.swift
+//
+// Created by Patrik Hora
+
+
+import UIKit
+
+
+class SceneDelegate: UIResponder, UIWindowSceneDelegate {
+    
+    var window: UIWindow?
+    
+    func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
+                
+        guard let windowScene = (scene as? UIWindowScene) else { return }
+        
+        self.window = UIWindow(windowScene: windowScene)
+        window?.rootViewController = DeviceListViewController()
+        window?.makeKeyAndVisible()
+    }
+}
